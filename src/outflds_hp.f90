30d23cb06a61ed2ecf05c3882e6ebe9674514503
xb118 <abc80943@cwb.gov.tw>
2019-06-28 16:23:50 +0800

    merge from r443, with origin mixpbl_new.f90, modified output include fv3 ini. data, 3hr precp., 1hr output, and rh10.

---
+++ src/outflds_hp.f90
@@ -0,0 +1,50 @@
+      subroutine outflds_hp( itau,nx,my,my_max,idtg,ifilout          &
+                           , raincu3,rainlp3,ggdef)
+      use mpe
+      use rank
+      use index
+
+      implicit  none
+
+      integer   itau,nx,my,my_max,istat,lenc
+
+      real      raincu3(nxp,my_max),rainlp3(nxp,my_max)
+
+      character ifilout*60, ggdef*4, ihdg*26
+      integer*8 idtg
+!
+! local work arrays
+!
+      real      glob(nx,my),glob1(nx,my) 
+
+      integer   jj,j,nxj,i
+!
+      lenc = nx*my
+!=======================================================================
+      if(myrank .eq. 0) print*,'   in outflds_hp for tau= ',itau
+!=======================================================================
+      call mpe2d_unify(glob,raincu3)
+      call mpe2d_unify(glob1,rainlp3)
+!      call mpe_unify_1(glob,raincu3,nx,my,2,mpe_double)
+!      call mpe_unify_1(glob1,rainlp3,nx,my,2,mpe_double)
+      call syslbl ('b00632',idtg,itau,ggdef,ihdg)
+      if( lreduce.eq.1 ) call reduceintp (glob,nxdef,nx,my)
+      call dmswrit(nx,my,ihdg,lenc,'H',ifilout,glob,istat)
+      call qmaxn3 (glob,ihdg(1:14),ihdg(15:26),1,1,1,nx,my,1)
+!
+      call syslbl ('b00642',idtg,itau,ggdef,ihdg)
+      if( lreduce.eq.1 ) call reduceintp (glob1,nxdef,nx,my)
+      call dmswrit(nx,my,ihdg,lenc,'H',ifilout,glob1,istat)
+      call qmaxn3 (glob1,ihdg(1:14),ihdg(15:26),1,1,1,nx,my,1)
+!
+      call syslbl ('b00622',idtg,itau,ggdef,ihdg)
+      do 98 j=1,my
+      do 98 i=1,nx
+       glob(i,j)=glob(i,j)+glob1(i,j)
+ 98   continue
+      call dmswrit(nx,my,ihdg,lenc,'H',ifilout,glob,istat)
+      call qmaxn3 (glob,ihdg(1:14),ihdg(15:26),1,1,1,nx,my,1)
+
+!=======================================================================
+      return
+      end
