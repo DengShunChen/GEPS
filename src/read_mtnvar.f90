cb6352e205861548115b1f02c30c56b02125dea3
Pang-Yen Liu <byliu@cwb.gov.tw>
2019-07-01 09:47:33 +0800

    --patching the files for new gravity wave drag--       new file:   gwdps.f90       new file:   read_mtnvar.f90

---
+++ src/read_mtnvar.f90
@@ -0,0 +1,51 @@
+      subroutine read_mtnvar(nx,my,mtnv,hprime_b)
+
+!
+      use index
+      use param ,only : my_max
+!
+!      parameter (nx=3072, my=1536, mtnv=14)
+!      parameter ( mtnv=14)
+      implicit none
+      integer i,j,v,jt,nrec,ii,jj,nxj,nx,my,mtnv
+      real*4 hprime_a(nx,my)
+      real hprime_b(nxp,mtnv,my_max),hprime_a8(nx,my)
+!      real hprime_a(nx,my),hprime_aa(nx,my)
+! 
+!--------------------------------------------
+      nrec=nx*my*4
+!!      open(22,file='/nwpr/gfs/p037/data/NCEPGFSGWDlzl/lzlwrkf2/terrain/'  &
+!!      //'terr_640.2576.1280/global_mtnvar.t640.2576.1280.f77',           &
+!!        form='unformatted',status='old',access='direct',recl=nrec )
+      open(22,file='mtnvar.dat',                                      &
+        form='unformatted',status='old',access='direct',recl=nrec )
+!     &   form='binary',status='old' )
+       do v =1,mtnv
+         read(22,rec=i) hprime_a
+         hprime_a8=hprime_a
+!      if( myrank .eq. 0 ) &
+!      print*,' in read_mtnvar hprime_a = ',(hprime_a(1500,155,i),i=1,mtnv)
+
+!--------------------------------------------
+
+        do jj = 1, jlistnum
+          j=jlist1(jj)
+          jt = my - j + 1
+          ii=nxjstart(j)
+          nxj=nxdef_2d(j)
+          if( lreduce.eq.1 )call reducepick (hprime_a8(1,jt),nxdef(j),nx,1)        
+          do i = 1, nxj
+            hprime_b(i,v,jj) = hprime_a8(ii,jt)
+            ii=ii+1
+          enddo
+        enddo
+      enddo
+
+!-- transpose even though glob 30" is from S to N and NCEP std is N to S
+
+
+!      if( myrank .eq. 0 ) &
+!      print*,'in read_mtnvar hprime_aa = ',(hprime_aa(1500,my-155+1,i),i=1,mtnv)
+!
+      return
+      end
