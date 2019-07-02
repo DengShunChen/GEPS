2b93c07a3d08c55c1cfc5119abfabddf6f0b43c0
xb118 <abc80943@cwb.gov.tw>
2019-07-02 09:46:06 +0800

    fix read_mtnvar.f90.

--- src/read_mtnvar.f90
+++ src/read_mtnvar.f90
@@ -1,8 +1,4 @@
-<<<<<<< HEAD
-     subroutine read_mtnvar(nx,my,mtnv,hprime_b)
-=======
       subroutine read_mtnvar(nx,my,mtnv,hprime_b)
->>>>>>> 64a9f40a91bbbba7e4796b7967f37f7109a84f91
 
 !
       use index
@@ -18,15 +14,8 @@
 ! 
 !--------------------------------------------
       nrec=nx*my*4
-<<<<<<< HEAD
-!!
-!open(22,file='/nwpr/gfs/p037/data/NCEPGFSGWDlzl/lzlwrkf2/terrain/'  &
-!!      //'terr_640.2576.1280/global_mtnvar.t640.2576.1280.f77',
-!&
-=======
 !!      open(22,file='/nwpr/gfs/p037/data/NCEPGFSGWDlzl/lzlwrkf2/terrain/'  &
 !!      //'terr_640.2576.1280/global_mtnvar.t640.2576.1280.f77',           &
->>>>>>> 64a9f40a91bbbba7e4796b7967f37f7109a84f91
 !!        form='unformatted',status='old',access='direct',recl=nrec )
       open(22,file='mtnvar.dat',                                      &
         form='unformatted',status='old',access='direct',recl=nrec )
@@ -35,12 +24,7 @@
          read(22,rec=v) hprime_a
          hprime_a8=hprime_a
 !      if( myrank .eq. 0 ) &
-<<<<<<< HEAD
-!      print*,' in read_mtnvar hprime_a =
-!      ',(hprime_a(1500,155,i),i=1,mtnv)
-=======
 !      print*,' in read_mtnvar hprime_a = ',(hprime_a(1500,155,i),i=1,mtnv)
->>>>>>> 64a9f40a91bbbba7e4796b7967f37f7109a84f91
 
 !--------------------------------------------
 
@@ -49,12 +33,8 @@
           jt = my - j + 1
           ii=nxjstart(j)
           nxj=nxdef_2d(j)
-<<<<<<< HEAD
           if( lreduce.eq.1 ) &
              call reducepick(hprime_a8(1,jt),nxdef(j),nx,1)        
-=======
-          if( lreduce.eq.1 )call reducepick (hprime_a8(1,jt),nxdef(j),nx,1)        
->>>>>>> 64a9f40a91bbbba7e4796b7967f37f7109a84f91
           do i = 1, nxj
             hprime_b(i,v,jj) = hprime_a8(ii,jt)
             ii=ii+1
@@ -66,12 +46,7 @@
 
 
 !      if( myrank .eq. 0 ) &
-<<<<<<< HEAD
-!      print*,'in read_mtnvar hprime_aa =
-!      ',(hprime_aa(1500,my-155+1,i),i=1,mtnv)
-=======
 !      print*,'in read_mtnvar hprime_aa = ',(hprime_aa(1500,my-155+1,i),i=1,mtnv)
->>>>>>> 64a9f40a91bbbba7e4796b7967f37f7109a84f91
 !
       return
       end
