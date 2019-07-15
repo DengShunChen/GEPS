ce8bb31772867528eb1ad1517cd91fff8a82b642
Pang-Yen Liu <byliu@cwb.gov.tw>
2019-07-15 16:15:06 +0800

    bug fixed for correcting the unit of upward surface latent heat flux from w/m2 to kgm^-2s^-1 by ChangHong Lin  Please enter the crmmit message for your changes. Lines starting

--- src/cumastr_driv.f90
+++ src/cumastr_driv.f90
@@ -146,7 +146,7 @@
 !c
 !      do  i=1,nx
       do  i=1,nxj
-      pqhfl(i)=qflux(i)
+      pqhfl(i)=qflux(i)/alv     !xb110 20190715, convert to upward surface moisture flux (kgm^-2s^-1)
       ldland(i)=land(i)
       enddo
 !c                                                                      c
