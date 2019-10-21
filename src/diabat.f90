7edf8fe43329da7ce764383d878eb6830504e698
Pang-Yen Liu <byliu@cwb.gov.tw>
2019-10-21 21:47:35 +0800

    --bug fixed-- define some variable in twice 	modified:   diabat.f90

--- src/diabat.f90
+++ src/diabat.f90
@@ -189,12 +189,7 @@
 !-----------------------------------------------------------------------
       integer nfxr, ntrac, kk, nk, n
       real slag,sdec,cdec,solcon,dtlw,dtsw,solhr
-
-
-! --- new variables setting :
-      integer*8 idtg
-      logical doo3l
-      integer ipt,jpt
+!
 ! for land_noah_new
        real      sfalb(nxp,my_max),sfemis(nxp,my_max)
        integer   isot,ivegsrc
