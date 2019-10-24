892d3fd9fd2a7ef100933084359087692864b8ea
Chang-Hung Lin <changhong@cwb.gov.tw>
2019-10-24 17:01:45 +0800

    bug fixed

--- src/lightning_ec.f90
+++ src/lightning_ec.f90
@@ -62,10 +62,10 @@
        ft = 0.
        do jl = 1,nxj
         if ( ldcum(jl) )then
-        do jk = 1,klev      
+        do jk = 1,klev-1      
 
           if (ztenh(jl,jk) .ge. 248.15 .and. ztenh(jl,jk) .le. 273.15) then
-            zdz = (pgeo(jl,jk-1)-pgeo(jl,jk))/g
+            zdz = (pgeo(jl,jk)-pgeo(jl,jk+1))/g              !xb110
             charg(jl) = charg(jl) + (qgraup(jl,jk)*                     &
                         (pluu(jl,jk) + qsnow(jl,jk)))*rho(jl,jk)*zdz
           end if
