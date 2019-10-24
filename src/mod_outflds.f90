d6128079aa10ff46a8b0949f46b67804b9eac535
Pang-Yen Liu <byliu@cwb.gov.tw>
2019-10-24 10:31:21 +0800

    --bug fixed--

--- src/mod_outflds.f90
+++ src/mod_outflds.f90
@@ -487,8 +487,9 @@ contains
 !
 !  sea surface level pressure
 !
-      call unify_reduceintp(nx,my,my_max,slp,glob)
+
       if(label(kk).eq.'SSL010' .or. label(kk).eq.'ssl010') then
+        call unify_reduceintp(nx,my,my_max,slp,glob)
         call syslbl('ssl010',idtg,itau,ggdef,lrec)
         if(lwrite) call dmswrit(nx,my,lrec,lenc,'H',ifilout,glob,istat)
         call qmaxn3(glob,lrec(1:14),lrec(15:26),1,1,1,nx,my,1)
