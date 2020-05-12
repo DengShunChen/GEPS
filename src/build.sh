9f82b847f8caab25957608a5cd96cbbb928c463e
Deng-Shun Chen <chendengshun@gmail.com>
2020-05-12 10:03:21 +0800

    move compile flag from build shell script to modulefiles

--- src/build.sh
+++ src/build.sh
@@ -19,40 +19,19 @@ else
   exit
 fi
 
+echo ${MACHINE} ${HOSTNAME}
+if [ "${MACHINE}" == 'fx10' ] &&  [ "${HOSTNAME}" != 'login07' ] && [ "${HOSTNAME}" != 'login08' ] && [ "${HOSTNAME}" != 'login05' ] && [ "${HOSTNAME}" != 'login06' ] ; then
+  echo "Build ${MACHINE} executable, please move to login07/08 for inside HPC, login05/06 for outside HPC !" ; exit
+fi
+if [ "${MACHINE}" == 'fx100' ] &&  [ "${HOSTNAME}" != 'login11' ] && [ "${HOSTNAME}" != 'login12' ] && [ "${HOSTNAME}" != 'login15' ] && [ "${HOSTNAME}" != 'login16' ]; then
+  echo "Build ${MACHINE} executable, please move to login11/12 for inside HPC, login15/16 for outside HPC !" ; exit
+fi
+
 # load libs
 export MDIR=$(cd ..;pwd)
 . /usr/share/Modules/init/bash
 module use ${MDIR}/modulefiles/${MACHINE}
 module add modulefile.tcogfs.${MACHINE}
-
-case $MACHINE in
-fx10)
-  export FFLAGS_COM="-c -X9 -Free -CcdRR8 -Cpp -Kfast,ocl,autoobjstack -x- -fw"
-  export DEFINE_FLAGS="-DTIMING -DUSE_FFTW -DW3TAG"
-  export MSG="-Ec -Qa,d,i,p,t,x -Koptmsg=2"
-  export LIBS_COM="-SSL2BLAMP"
-  export OMP="-Kopenmp,parallel"
-;;
-fx100)
-  export FFLAGS_COM="-c -X9 -Free -CcdRR8 -Cpp -Kfast,ocl,autoobjstack -x- -fw"
-  export DEFINE_FLAGS="-DTIMING -DUSE_FFTW -DW3TAG"
-  export MSG="-Ec -Qa,d,i,p,t,x -Koptmsg=2"
-  export LIBS_COM="-SSL2BLAMP"
-  export OMP="-Kopenmp,parallel"
-;;
-pcc)
-  export FFLAGS_COM="-c -O3 -Free -r8 -fpp -convert big_endian -w -ftz -ip -fp-model precise -align all -fno-alias -FR -xHost -fp-model fast=2 -no-heap-arrays -no-prec-div -no-prec-sqrt -fno-common -xCORE-AVX2 -auto"
-  export DEFINE_FLAGS="-DTIMING -DUSE_FFTW "
-  export MSG=""
-  export LIBS_COM="-mkl=cluster -qopt-matmul"
-  export LDFLAGS_COM='-static-intel -qopt-report'
-  export OMP="-parallel"
-;;
-*)
-  echo "${0} : Unknown machine --> ${MACHINE}" ; exit
-;;
-
-esac
 
 make clean
 make -j24
