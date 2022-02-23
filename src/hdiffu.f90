6feba66421f3e4fe5012098fe16db395bb1fc2ae
Pang-Yen Liu <byliu@cwb.gov.tw>
2022-02-23 10:26:06 +0800

    [update]1. added the options of 128 levels         2. fixed the unstable problem at tropopause.

--- src/hdiffu.f90
+++ src/hdiffu.f90
@@ -64,8 +64,8 @@
       factop = 1.5
       coefu = factop/float(hdk2(3)-hdk2(2))
       if ( octahedral ) then
-        hfilt  = hfilt/(9.*dta)
-        hfilt2 = hfilt2/(9.*dta)
+        hfilt  = hfilt/(6.*dta)
+        hfilt2 = hfilt2/(6.*dta)
       else
         hfilt  = 16.*hfilt/dta
         hfilt2 = 16.*hfilt2/dta
@@ -77,7 +77,7 @@
 !
         KL=Llist(k)
 !
-        kfac  = (5.+finc)*max(float(hdk2(1)-KL),0.)
+        kfac  = (1.+finc)*max(float(hdk2(1)-KL),0.)
         facd = max(amp,kfac)
         facv = max(min(amp,1.),kfac)
         fact = max(min(amp,1.),kfac)
