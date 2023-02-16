8be26e3b35b77f0b9779a91f641ea0b455a45766
xb141 <davilina0422@cwb.gov.tw>
2023-02-16 16:05:28 +0800

    add vapor condensed factor (fac_qsw) for GFDL MP v1

--- src/mp_scheme.f90
+++ src/mp_scheme.f90
@@ -527,6 +527,10 @@
         phys_hydrostatic = .true.   !flag for hydrostatic heating from physics 
         sedi_w = .false.
 
+      ! define factor of vapor condensed threshold
+!        fac_qsw = 1.0                 ! default
+        fac_qsw = 1.0 - 0.02*cosl**2  ! =0.98 at equator; =1.0 at pole
+
         do i = 1, nxj
           if( islimsk(i) .eq. 1 ) land2d(i,1) = 1.  !land fraction
           if( effr_in ) mask1d(i) = islimsk(i)      !land-sea mask
@@ -567,7 +571,7 @@
                   cldten3d, tten3d, t3d, w3d, u3d, v3d, uten3d, vten3d, &
                   dz3d, dp3d, garea, dta, land2d,                       &
                   rain2d, snow2d, ice2d, graupel2d,                     &
-                  hydrostatic, phys_hydrostatic,                        &
+                  fac_qsw, hydrostatic, phys_hydrostatic,               &
                   1, nxj, 1, 1, 1, lev, 1, lev )
 
         do k = 1, lev
