f361dd592bf8628fff2ff2f4bac37dec026e640d
Chong-Wei, Huang <crazy10167@cwb.gov.tw>
2023-10-18 12:12:33 +0800

    albedo unit change move to readalb inside

--- src/getrdy.f90
+++ src/getrdy.f90
@@ -360,18 +360,6 @@
         call readalb(bckfile,nx,my,my_max,julian,ggdef,         &
                    alvsf,alvwf,alnsf,alnwf,facsf,facwf)
         if (myrank.eq.0) print *, 'irad=2, readalb ok!!'
-        do jj=1,jlistnum
-          j=jlist1(jj)
-          nxj=nxdef_2d(j)
-          do i=1,nxj
-            alvsf(i,jj)=alvsf(i,jj)*0.01
-            alvwf(i,jj)=alvwf(i,jj)*0.01
-            alnsf(i,jj)=alnsf(i,jj)*0.01
-            alnwf(i,jj)=alnwf(i,jj)*0.01
-            facsf(i,jj)=facsf(i,jj)*0.01
-            facwf(i,jj)=facwf(i,jj)*0.01
-          enddo
-        enddo
       endif
 ! ------------------------------------------------------------
       if(.not.cstar)then
