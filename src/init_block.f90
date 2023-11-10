67c968e9f89c0aa4b13894bf4821722b6b1c5c86
Brian Liu <byliu@cwb.gov.tw>
2023-11-10 21:00:34 +0800

    [update] applied new treatment for horizontal diffusion over model top

--- src/init_block.f90
+++ src/init_block.f90
@@ -202,7 +202,7 @@
 !  sponge layer 
       spl1=10.
       spl2=100.
-      vd=0.08
+      vd=0.02
       else if ( lev .eq. 72 ) then
 !
 ! L72 hybrid coordinate
@@ -251,7 +251,7 @@
 !  sponge layer 
       spl1=5.
       spl2=50.
-      vd=0.08
+      vd=0.02
       else if ( lev .eq. 128 ) then
 !
 ! L128 hybrid coordinate
@@ -328,7 +328,7 @@
 !  sponge layer 
       spl1=1.
       spl2=50.
-      vd=0.1
+      vd=0.03
       endif
 !
       tmeans=300.
