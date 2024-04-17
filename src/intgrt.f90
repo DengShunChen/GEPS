a7ab593cff725936426ad7497283051ed842be44
Hsi-Hsien Tseng <shishian1996@cwb.gov.tw>
2024-04-17 16:05:54 +0800

    Merge branch 'GEPSdev' into Timcomg (GEPSv2.1.1 - GEPSv3)

--- src/intgrt.f90
+++ src/intgrt.f90
@@ -989,11 +989,7 @@
       enddo
       call transr1(jtrun,jtmax,nx,my,my_max,poly,pltemp,pt,nsizey)
 
-<<<<<<< HEAD
-!      if ( mass_dp ) then
-=======
       !mass conservation
->>>>>>> GEPSdev
       call ptotc(pdry,dpprt)
       pcorr = (pdryi-pdry) * sqrt(2.)
 !
