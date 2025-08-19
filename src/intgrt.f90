15bc3bfa42af4948bfd749015ee84af3d2dfbe83
Hsi-Hsien Tseng <shishian1996@cwa.gov.tw>
2025-08-19 07:40:33 +0000

    New branch

--- src/intgrt.f90
+++ src/intgrt.f90
@@ -184,7 +184,7 @@
       real, dimension(nxp, my_max) :: tocn_cpl, uocn_cpl, vocn_cpl, &
                             u10m_cpl, v10m_cpl, t02m_cpl, q02m_cpl, &
                             pslv_cpl, swup_cpl, swdn_cpl, lwdn_cpl, &
-                            rain_cpl, snow_cpl, tgfs_cpl, tg_ocn, tg_diff
+                            rain_cpl, snow_cpl, tgfs_cpl
       real :: dt_cpl, tgwf, cplf
       logical :: cpl_send_init
       integer :: compid
@@ -283,8 +283,8 @@
       snow_cpl = 0.
         dt_cpl = 0.
       cpl_send_init = .true.
+      if(.not. restrt) &
          tg_ocn= 0.
-        tg_diff=0.
            cplf=2.0 !cpl frequency
 #endif
 !
@@ -485,25 +485,13 @@
           end if   !end lopgsst
 !
 #ifdef TIMCOMCPL
-      call gfs_cpl_recv4gocn(compid, land, ice, tg_ocn, ssu, ssv)
-!      if(myrank .eq. 0) then
-!      write(*,*) "no replace the tg at first step"
-!        do i = 1,100
-!        do j = 1,100
-!        write(*,*) 'tg_ocn=', tg_ocn(2,7) !, "i=", i, "j=", j
-!        write(*,*) 'tg=', tg(2,7) !, "i=", i, "j=", j
-!        write(*,*) 'sssssu(max)=', maxval(ssu) ,'rank=', myrank
-!        write(*,*) 'sssssu(min)=', minval(ssu) ,'rank=', myrank
-!        write(*,*) 'sssssv(max)=', maxval(ssv)
-!        write(*,*) 'sssssv(min)=', minval(ssv)
-!       enddo
-!       enddo
+      if(.not. restrt) then
+        call gfs_cpl_recv4gocn(compid, land, ice, tg_ocn, ssu, ssv)
         tgwf=1.0/(86400.0/dta/(24.0/cplf)-1.0)
+        tg_diff=0.
         do i = 1,nxp
          do j = 1,my_max
-          if(tg_ocn(i,j).eq.0.0) then
-            tg_diff(i,j)=0.0
-          else 
+          if(tg_ocn(i,j).ne.0.0) then
             tg_diff(i,j)=(tg_ocn(i,j)-tg(i,j))*tgwf
           end if
          end do
@@ -511,7 +499,7 @@
       if(myrank .eq. 0) then
             write(*,*) '1loop tg_diff=',tg_diff(2,7)
       endif
-
+      endif
 #endif 
 
  10   continue
@@ -1461,6 +1449,93 @@
                 end if
              end do
           end do
+!    ---------------------------------------------------------------
+#ifdef TIMCOMCPL
+          u10m_cpl = u10m_cpl + u10*dtx
+          v10m_cpl = v10m_cpl + v10*dtx
+          t02m_cpl = t02m_cpl +  t2*dtx
+          q02m_cpl = q02m_cpl +  q2*dtx
+          pslv_cpl = pslv_cpl + (pt+pdiff)*dtx
+          swup_cpl = swup_cpl + (ss-sld)*dtx
+          swdn_cpl = swdn_cpl + sld*dtx
+          lwdn_cpl = lwdn_cpl + rld*dtx
+          rain_cpl = rain_cpl + totalp
+          snow_cpl = snow_cpl
+          dt_cpl   = dt_cpl + dtx
+          if(cpl_send_init)then
+            if(.not.restrt) then
+              call gfs_cpl_send2gocn(compid,  u10m_cpl/dtx, v10m_cpl/dtx, &
+                                              t02m_cpl/dtx, q02m_cpl/dtx, &
+                                              pslv_cpl/dtx, swup_cpl/dtx, &
+                                              swdn_cpl/dtx, lwdn_cpl/dtx, &
+                                              rain_cpl/dtx, snow_cpl/dtx, tg)
+            endif
+            cpl_send_init = .false.
+          endif
+
+          dtaup = mod(tau+0.001, 2.0)
+          if( dtaup .lt. dtx_tau ) then
+            if(myrank .eq. 0) write(*,*) "TCo time to coupler", tau
+            call gfs_cpl_recv4gocn(compid, land, ice, tg_ocn, ssu, ssv)
+            u10m_cpl = u10m_cpl/dt_cpl
+            v10m_cpl = v10m_cpl/dt_cpl
+            t02m_cpl = t02m_cpl/dt_cpl
+            q02m_cpl = q02m_cpl/dt_cpl
+            pslv_cpl = pslv_cpl/dt_cpl
+            swup_cpl = swup_cpl/dt_cpl
+            swdn_cpl = swdn_cpl/dt_cpl
+            lwdn_cpl = lwdn_cpl/dt_cpl
+            rain_cpl = rain_cpl/dt_cpl
+            snow_cpl = snow_cpl/dt_cpl
+            call gfs_cpl_send2gocn(compid, u10m_cpl, v10m_cpl, &
+                                           t02m_cpl, q02m_cpl, &
+                                           pslv_cpl, swup_cpl, &
+                                           swdn_cpl, lwdn_cpl, &
+                                           rain_cpl, snow_cpl, tg)
+            u10m_cpl = 0.
+            v10m_cpl = 0.
+            t02m_cpl = 0.
+            q02m_cpl = 0.
+            pslv_cpl = 0.
+            swup_cpl = 0.
+            swdn_cpl = 0.
+            lwdn_cpl = 0.
+            rain_cpl = 0.
+            snow_cpl = 0.
+            dt_cpl   = 0.
+
+            tgwf=1.0/(86400.0/dta/(24.0/cplf))
+
+            tg_diff=0.
+            do j = 1,my_max
+             do i = 1,nxp
+!             if(tg_ocn(i,j).eq.0.0) then
+!               tg_diff(i,j)=0.0
+!             else
+              if(tg_ocn(i,j).ne.0.0) then
+                tg_diff(i,j)=(tg_ocn(i,j)-tg(i,j))*tgwf
+              end if
+             end do
+            end do
+            if(myrank .eq. 0) then
+                write(*,*) '2 loop tg_diff=',tg_diff(2,7)
+            end if
+          end if
+
+!         if(myrank .eq. 0) then
+!           write(*,*) 'tg_ocn=', tg_ocn(2,7) !, "i=", i, "j=", j
+!           write(*,*) 'tg=', tg(2,7) !, "i=", i, "j=", j
+!           write(*,*) 'tg_atm=', tg(2,1) !, "i=", i, "j=", j
+!         end if
+          tg=tg_diff+tg
+!         if(myrank .eq. 0) then
+!           write(*,*) '------after-------'
+!           write(*,*) 'tg=', tg(2,7)
+!           write(*,*) 'tg_atm=', tg(2,1) !, "i=", i, "j=", j
+!         end if
+
+#endif
+!
 
 !    ---------------------------------------------------------------
 !     check tau in hourly for output
@@ -1525,6 +1600,7 @@
                 if (wrestrt) then
 ! set write out restart at the end of integration
                    if (mod(float(itau), float(itauezz)) .lt. 0.01) then
+                      if(myrank.eq.0) write(*,*) 'Write out restart files in itau=',itau
                       write (ctau, 800) itau
 800                   format(i7.7)
                       write (ccore, '(i4.4)') myrank
@@ -1577,6 +1653,8 @@
                       write (i) sncover
                       write (i) sndepth
                       write (i) tg
+                      write (i) tg_diff
+                      write (i) tg_ocn
                       write (i) gwr
                       write (i) gwet
                       write (i) cice
@@ -1641,6 +1719,10 @@
                       write (i) smc
                       write (i) stc
                       write (i) slc
+#ifdef TIMCOMCPL
+                      write (i) ssu
+                      write (i) ssv
+#endif
                       close (i)
                    end if ! end of ( mod(float(itau),float(itauezz)) .lt. 0.01 )
                    if (do_sit) then
@@ -1945,88 +2027,88 @@
         endif
       endif   !end lopgsst
 !
-#ifdef TIMCOMCPL
-      u10m_cpl = u10m_cpl + u10*dtx
-      v10m_cpl = v10m_cpl + v10*dtx
-      t02m_cpl = t02m_cpl +  t2*dtx
-      q02m_cpl = q02m_cpl +  q2*dtx
-      pslv_cpl = pslv_cpl + (pt+pdiff)*dtx
-      swup_cpl = swup_cpl + (ss-sld)*dtx
-      swdn_cpl = swdn_cpl + sld*dtx
-      lwdn_cpl = lwdn_cpl + rld*dtx
-      rain_cpl = rain_cpl + totalp
-      snow_cpl = snow_cpl
-      dt_cpl   = dt_cpl + dtx
-      if(cpl_send_init)then
-        call gfs_cpl_send2gocn(compid,  u10m_cpl/dtx, v10m_cpl/dtx, &
-                                        t02m_cpl/dtx, q02m_cpl/dtx, &
-                                        pslv_cpl/dtx, swup_cpl/dtx, &
-                                        swdn_cpl/dtx, lwdn_cpl/dtx, &
-                                        rain_cpl/dtx, snow_cpl/dtx, tg)
-        cpl_send_init = .false.
-      endif
+!#ifdef TIMCOMCPL
+!     u10m_cpl = u10m_cpl + u10*dtx
+!     v10m_cpl = v10m_cpl + v10*dtx
+!     t02m_cpl = t02m_cpl +  t2*dtx
+!     q02m_cpl = q02m_cpl +  q2*dtx
+!     pslv_cpl = pslv_cpl + (pt+pdiff)*dtx
+!     swup_cpl = swup_cpl + (ss-sld)*dtx
+!     swdn_cpl = swdn_cpl + sld*dtx
+!     lwdn_cpl = lwdn_cpl + rld*dtx
+!     rain_cpl = rain_cpl + totalp
+!     snow_cpl = snow_cpl
+!     dt_cpl   = dt_cpl + dtx
+!     if(cpl_send_init)then
+!       call gfs_cpl_send2gocn(compid,  u10m_cpl/dtx, v10m_cpl/dtx, &
+!                                       t02m_cpl/dtx, q02m_cpl/dtx, &
+!                                       pslv_cpl/dtx, swup_cpl/dtx, &
+!                                       swdn_cpl/dtx, lwdn_cpl/dtx, &
+!                                       rain_cpl/dtx, snow_cpl/dtx, tg)
+!       cpl_send_init = .false.
+!     endif
 
-      dtaup = mod(tau+0.001, cplf)
-      if( dtaup .lt. dtx_tau ) then
-        if(myrank .eq. 0) write(*,*) "TCo time to coupler", tau
+!     dtaup = mod(tau+0.001, cplf)
+!     if( dtaup .lt. dtx_tau ) then
+!       if(myrank .eq. 0) write(*,*) "TCo time to coupler", tau
 
-        call gfs_cpl_recv4gocn(compid, land, ice, tg_ocn, ssu, ssv)
-        u10m_cpl = u10m_cpl/dt_cpl
-        v10m_cpl = v10m_cpl/dt_cpl
-        t02m_cpl = t02m_cpl/dt_cpl
-        q02m_cpl = q02m_cpl/dt_cpl
-        pslv_cpl = pslv_cpl/dt_cpl
-        swup_cpl = swup_cpl/dt_cpl
-        swdn_cpl = swdn_cpl/dt_cpl
-        lwdn_cpl = lwdn_cpl/dt_cpl
-        rain_cpl = rain_cpl/dt_cpl
-        snow_cpl = snow_cpl/dt_cpl
-        call gfs_cpl_send2gocn(compid, u10m_cpl, v10m_cpl, &
-                                       t02m_cpl, q02m_cpl, &
-                                       pslv_cpl, swup_cpl, &
-                                       swdn_cpl, lwdn_cpl, &
-                                       rain_cpl, snow_cpl, tg)
-        u10m_cpl = 0.
-        v10m_cpl = 0.
-        t02m_cpl = 0.
-        q02m_cpl = 0.
-        pslv_cpl = 0.
-        swup_cpl = 0.
-        swdn_cpl = 0.
-        lwdn_cpl = 0.
-        rain_cpl = 0.
-        snow_cpl = 0.
-        dt_cpl   = 0.
+!       call gfs_cpl_recv4gocn(compid, land, ice, tg_ocn, ssu, ssv)
+!       u10m_cpl = u10m_cpl/dt_cpl
+!       v10m_cpl = v10m_cpl/dt_cpl
+!       t02m_cpl = t02m_cpl/dt_cpl
+!       q02m_cpl = q02m_cpl/dt_cpl
+!       pslv_cpl = pslv_cpl/dt_cpl
+!       swup_cpl = swup_cpl/dt_cpl
+!       swdn_cpl = swdn_cpl/dt_cpl
+!       lwdn_cpl = lwdn_cpl/dt_cpl
+!       rain_cpl = rain_cpl/dt_cpl
+!       snow_cpl = snow_cpl/dt_cpl
+!       call gfs_cpl_send2gocn(compid, u10m_cpl, v10m_cpl, &
+!                                      t02m_cpl, q02m_cpl, &
+!                                      pslv_cpl, swup_cpl, &
+!                                      swdn_cpl, lwdn_cpl, &
+!                                      rain_cpl, snow_cpl, tg)
+!       u10m_cpl = 0.
+!       v10m_cpl = 0.
+!       t02m_cpl = 0.
+!       q02m_cpl = 0.
+!       pslv_cpl = 0.
+!       swup_cpl = 0.
+!       swdn_cpl = 0.
+!       lwdn_cpl = 0.
+!       rain_cpl = 0.
+!       snow_cpl = 0.
+!       dt_cpl   = 0.
 
-        tgwf=1.0/(86400.0/dta/(24.0/cplf))
+!       tgwf=1.0/(86400.0/dta/(24.0/cplf))
 
-        do i = 1,nxp
-         do j = 1,my_max
-          if(tg_ocn(i,j).eq.0.0) then
-            tg_diff(i,j)=0.0
-          else
-            tg_diff(i,j)=(tg_ocn(i,j)-tg(i,j))*tgwf
-          end if
-         end do
-        end do
-        if(myrank .eq. 0) then
-            write(*,*) '2 loop tg_diff=',tg_diff(2,7)
-        end if
-      end if
+!       do i = 1,nxp
+!        do j = 1,my_max
+!         if(tg_ocn(i,j).eq.0.0) then
+!           tg_diff(i,j)=0.0
+!         else
+!           tg_diff(i,j)=(tg_ocn(i,j)-tg(i,j))*tgwf
+!         end if
+!        end do
+!       end do
+!       if(myrank .eq. 0) then
+!           write(*,*) '2 loop tg_diff=',tg_diff(2,7)
+!       end if
+!     end if
 
-      if(myrank .eq. 0) then
-        write(*,*) 'tg_ocn=', tg_ocn(2,7) !, "i=", i, "j=", j
-        write(*,*) 'tg=', tg(2,7) !, "i=", i, "j=", j
-        write(*,*) 'tg_atm=', tg(2,1) !, "i=", i, "j=", j
-      end if
-        tg=tg_diff+tg
-      if(myrank .eq. 0) then
-        write(*,*) '------after-------'
-        write(*,*) 'tg=', tg(2,7)
-        write(*,*) 'tg_atm=', tg(2,1) !, "i=", i, "j=", j
-      end if
+!     if(myrank .eq. 0) then
+!       write(*,*) 'tg_ocn=', tg_ocn(2,7) !, "i=", i, "j=", j
+!       write(*,*) 'tg=', tg(2,7) !, "i=", i, "j=", j
+!       write(*,*) 'tg_atm=', tg(2,1) !, "i=", i, "j=", j
+!     end if
+!       tg=tg_diff+tg
+!     if(myrank .eq. 0) then
+!       write(*,*) '------after-------'
+!       write(*,*) 'tg=', tg(2,7)
+!       write(*,*) 'tg_atm=', tg(2,1) !, "i=", i, "j=", j
+!     end if
 
-#endif
+!#endif
 
       itau=tau+0.001
 #ifdef TIMING
