5de6631b807698494a3f653721ddc75760aa32c1
shishian1996 <shishian1996@gmail.com>
2025-10-27 15:54:18 +0800

    change coupler path

--- build.sh
+++ build.sh
@@ -46,12 +46,12 @@ case "$OPTION" in
   2cpl)
     module load modulefile.tcogfs.${MACHINE}_2cpl
     export TIMCOMCPL=TRUE
-    export clpath=${MDIR}/../coupler
+    export clpath=${MDIR}/../coupler/fx1000
     ;;
   4cpl)
     module load modulefile.tcogfs.${MACHINE}_4cpl
     export TIMCOMCPL=TRUE
-    export clpath=/data/common/gfs/GEPSv3_lib/coupler
+    export clpath=${MDIR}/../coupler/fx1000
     ;;
   "")
     module load modulefile.tcogfs.${MACHINE}
