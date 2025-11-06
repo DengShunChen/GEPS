f23bc87b45c94421fbbfd208617eab51c11a6a01
shishian1996 <shishian1996@gmail.com>
2025-11-06 10:35:39 +0800

    fixed intgrt/intgrt_gpu

--- src/nvidia/intgrt_gpu.f90
+++ src/nvidia/intgrt_gpu.f90
@@ -204,7 +204,7 @@
       real, dimension(nxp, my_max) :: tocn_cpl, uocn_cpl, vocn_cpl, &
                             u10m_cpl, v10m_cpl, t02m_cpl, q02m_cpl, &
                             pslv_cpl, swup_cpl, swdn_cpl, lwdn_cpl, &
-                            rain_cpl, snow_cpl, tgfs_cpl, tg_ocn, tg_diff
+                            rain_cpl, snow_cpl, tgfs_cpl
       real :: dt_cpl, tgwf, cplf
       logical :: cpl_send_init
       integer :: compid
@@ -629,7 +629,7 @@ if(.not. restrt) then
       if(myrank .eq. 0) then
             write(*,*) '1loop tg_diff=',tg_diff(2,7)
       endif
-
+endif
 #endif
 
 10 continue
@@ -2389,95 +2389,6 @@ if(.not. restrt) then
       end if
    end if   !end lopgsst
 !
-#ifdef TIMCOMCPL
-!$acc wait(async_id)
-!$acc update self(totalp, land, ice, tg, sld, ss, rld, t2, q2, u10, v10) async(async_id)
-!$acc wait(async_id)
-      u10m_cpl = u10m_cpl + u10*dtx
-      v10m_cpl = v10m_cpl + v10*dtx
-      t02m_cpl = t02m_cpl +  t2*dtx
-      q02m_cpl = q02m_cpl +  q2*dtx
-      pslv_cpl = pslv_cpl + (pt+pdiff)*dtx
-      swup_cpl = swup_cpl + (ss-sld)*dtx
-      swdn_cpl = swdn_cpl + sld*dtx
-      lwdn_cpl = lwdn_cpl + rld*dtx
-      rain_cpl = rain_cpl + totalp
-      snow_cpl = snow_cpl
-      dt_cpl   = dt_cpl + dtx
-      if(cpl_send_init)then
-        call gfs_cpl_send2gocn(compid,  u10m_cpl/dtx, v10m_cpl/dtx, &
-                                        t02m_cpl/dtx, q02m_cpl/dtx, &
-                                        pslv_cpl/dtx, swup_cpl/dtx, &
-                                        swdn_cpl/dtx, lwdn_cpl/dtx, &
-                                        rain_cpl/dtx, snow_cpl/dtx, tg)
-        cpl_send_init = .false.
-      endif
-
-      dtaup = mod(tau+0.001, cplf)
-      if( dtaup .lt. dtx_tau ) then
-        if(myrank .eq. 0) write(*,*) "TCo time to coupler", tau
-
-        call gfs_cpl_recv4gocn(compid, land, ice, tg_ocn, ssu, ssv)
-        u10m_cpl = u10m_cpl/dt_cpl
-        v10m_cpl = v10m_cpl/dt_cpl
-        t02m_cpl = t02m_cpl/dt_cpl
-        q02m_cpl = q02m_cpl/dt_cpl
-        pslv_cpl = pslv_cpl/dt_cpl
-        swup_cpl = swup_cpl/dt_cpl
-        swdn_cpl = swdn_cpl/dt_cpl
-        lwdn_cpl = lwdn_cpl/dt_cpl
-        rain_cpl = rain_cpl/dt_cpl
-        snow_cpl = snow_cpl/dt_cpl
-        call gfs_cpl_send2gocn(compid, u10m_cpl, v10m_cpl, &
-                                       t02m_cpl, q02m_cpl, &
-                                       pslv_cpl, swup_cpl, &
-                                       swdn_cpl, lwdn_cpl, &
-                                       rain_cpl, snow_cpl, tg)
-        u10m_cpl = 0.
-        v10m_cpl = 0.
-        t02m_cpl = 0.
-        q02m_cpl = 0.
-        pslv_cpl = 0.
-        swup_cpl = 0.
-        swdn_cpl = 0.
-        lwdn_cpl = 0.
-        rain_cpl = 0.
-        snow_cpl = 0.
-        dt_cpl   = 0.
-
-        tgwf=1.0/(86400.0/dta/(24.0/cplf))
-
-        do i = 1,nxp
-         do j = 1,my_max
-          if(tg_ocn(i,j).eq.0.0) then
-            tg_diff(i,j)=0.0
-          else
-            tg_diff(i,j)=(tg_ocn(i,j)-tg(i,j))*tgwf
-          end if
-         end do
-        end do
-        if(myrank .eq. 0) then
-            write(*,*) '2 loop tg_diff=',tg_diff(2,7)
-        end if
-      end if
-
-      if(myrank .eq. 0) then
-        write(*,*) 'tg_ocn=', tg_ocn(2,7) !, "i=", i, "j=", j
-        write(*,*) 'tg=', tg(2,7) !, "i=", i, "j=", j
-        write(*,*) 'tg_atm=', tg(2,1) !, "i=", i, "j=", j
-      end if
-        tg=tg_diff+tg
-      if(myrank .eq. 0) then
-        write(*,*) '------after-------'
-        write(*,*) 'tg=', tg(2,7)
-        write(*,*) 'tg_atm=', tg(2,1) !, "i=", i, "j=", j
-      end if
-!$acc wait(async_id)
-!$acc update device(tg) async(async_id)
-!$acc wait(async_id)
-
-#endif
-
 
    itau = tau + 0.001
 #ifdef TIMING
