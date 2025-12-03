45b0d42fcc1701b1613e7c7c79102de56cf24661
尚恩 李 <fish5045@cwa.gov.tw>
2025-12-03 01:59:14 +0000

    Change UT job queue from large to small to run CICD

--- job/unit_test.sh
+++ job/unit_test.sh
@@ -12,5 +12,5 @@ if [ $(id -gn) != $group ]; then
   exec sg $group "$0 $*"
 fi
 
-pjsub -N unit-test -L "vnode=1,vnode-core=32,ru=rscunit_pg01,rg=gpu-rd-large,gpu=8" --sparam wait-time=100 --interact -g ${group} qa/L0_test_subroutine/test.sh
+pjsub -N unit-test -L "vnode=1,vnode-core=32,ru=rscunit_pg01,rg=gpu-rd-small,gpu-share=8" --sparam wait-time=100 --interact -g ${group} qa/L0_test_subroutine/test.sh
 
